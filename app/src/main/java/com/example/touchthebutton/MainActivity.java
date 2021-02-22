6aa8c5d0a518a59008554560e12ac17b8a4026cf
Иван <zhivancomp19@gmail.com>
2021-02-22 20:42:18 +0300

    Отрисовали кнопки

---
+++ app/src/main/java/com/example/touchthebutton/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.touchthebutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button Button1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button1 = findViewById(R.id.result_Button);
+    }
+
+
+    public void clickButton(View v) {
+
+    }
+}
