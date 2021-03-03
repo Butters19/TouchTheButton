d243da61ef0cb2f8966d5308b38f64decbd855a7
Иван <zhivancomp19@gmail.com>
2021-03-04 00:04:48 +0300

    Немного рефакторинга

--- app/src/main/java/com/example/touchthebutton/MainActivity.java
+++ app/src/main/java/com/example/touchthebutton/MainActivity.java
@@ -14,58 +14,35 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
-    Button button1, btnActTwo;
     ClickModel model;
-    TextView counterTitleTextView, counterTextView, lastDateTitleTextView, lastDateValueTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         model = new ClickModel();
         setContentView(R.layout.activity_main);
-        button1 = findViewById(R.id.result_Button);
-        btnActTwo = findViewById(R.id.info_Button);
-        counterTitleTextView = findViewById(R.id.counterTitle);
-        counterTextView = findViewById(R.id.counterTextView);
-        lastDateTitleTextView = findViewById(R.id.lastDateTitle);
-        lastDateValueTextView = findViewById(R.id.lastDateValue);
 
-
-        btnActTwo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putStringArrayListExtra("key", model.list);
-                startActivity(intent);
-            }
+        findViewById(R.id.info_Button).setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            intent.putStringArrayListExtra("key", model.list);
+            startActivity(intent);
         });
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                model.list.add(getCurrentDate());
-                counterTitleTextView.setVisibility(View.VISIBLE);
-                counterTextView.setVisibility(View.VISIBLE);
-                lastDateTitleTextView.setVisibility(View.VISIBLE);
-                lastDateValueTextView.setVisibility(View.VISIBLE);
-                counterTitleTextView.setText("Count of click:");
-                lastDateTitleTextView.setText("Last click date:");
-                counterTextView.setText(String.valueOf(model.list.size()));
-                lastDateValueTextView.setText(String.valueOf(model.list.get(model.list.size() - 1)));
-
-            }
+        findViewById(R.id.result_Button).setOnClickListener(v -> {
+            model.list.add(getCurrentDate());
+            setValueToFiled(findViewById(R.id.counterTitle),"Count of click:");
+            setValueToFiled(findViewById(R.id.counterTextView),String.valueOf(model.list.size()));
+            setValueToFiled(findViewById(R.id.lastDateTitle),"Last click date:");
+            setValueToFiled(findViewById(R.id.lastDateValue),String.valueOf(model.list.get(model.list.size() - 1)));
         });
+    }
+
+    public void setValueToFiled(TextView view, String value){
+        view.setVisibility(View.VISIBLE);
+        view.setText(value);
     }
 
     public String getCurrentDate(){
-        SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:S");
-        Date date = new Date();
-        date.getTime();
-        return formater.format(date);
-    }
-
-
-    public void clickButton(View v) {
-
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:S").format( new Date().getTime());
     }
 }
